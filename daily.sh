5adf88ef599e810af7677d9dc78570151489afb2
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2022-07-12 08:31:56 +0800

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -2,11 +2,11 @@
 
 rm -f /var/lib/docker/volumes/htmls/_data/*
 
-docker run --name crawler --net=container:bitcoin_mysql crawler:1.0.0 >> /tmp/crawler.txt
-docker run --name etl --net=container:bitcoin_mysql etl:1.0.0 >> /tmp/etl.txt
-docker run --name predict --net=container:bitcoin_mysql predict:1.0.0 >> /tmp/predict.txt
-docker run --name result --net=container:bitcoin_mysql result:1.0.0 >> /tmp/result.txt
-docker run --name charts --net=container:bitcoin_mysql -v htmls:/charts/templates/ charts:1.0.0 >> /tmp/charts.txt
-docker run --name webapp -d --net=host -v htmls:/webapp/templates/ webapp:1.0.0 >> /tmp/webapp.txt
+docker run --name crawler --net=container:bitcoin_mysql crawler >> /tmp/crawler.txt
+docker run --name etl --net=container:bitcoin_mysql etl >> /tmp/etl.txt
+docker run --name predict --net=container:bitcoin_mysql predict >> /tmp/predict.txt
+docker run --name result --net=container:bitcoin_mysql result >> /tmp/result.txt
+docker run --name charts --net=container:bitcoin_mysql -v htmls:/charts/templates/ charts >> /tmp/charts.txt
+docker run --name webapp -d --net=host -v htmls:/webapp/templates/ webapp >> /tmp/webapp.txt
 
 docker rm -f $(docker ps -qf status=exited)
