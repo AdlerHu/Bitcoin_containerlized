4f34a3ea0f2f5677a96e0360a00db0af6bcbd0a7
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2022-07-12 08:22:13 +0800

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -3,6 +3,9 @@
 apt-get update
 apt-get upgrade -y
 
+# sset hostname
+echo "linode" > /etc/hostname
+
 # install Docker
 apt-get install  -y \
     ca-certificates \
@@ -31,3 +34,60 @@ docker pull adlerhu/bitcoin_predict
 docker pull adlerhu/bitcoin_result
 docker pull adlerhu/bitcoin_charts
 docker pull adlerhu/bitcoin_webapp
+
+# daily job
+touch /root/daily.sh
+echo "#!/bin/sh
+
+rm -f /var/lib/docker/volumes/htmls/_data/*
+
+docker run --name crawler --net=container:bitcoin_mysql adlerhu/bitcoin_crawler >> /tmp/crawler.txt
+docker run --name etl --net=container:bitcoin_mysql adlerhu/bitcoin_etl >> /tmp/etl.txt
+docker run --name predict --net=container:bitcoin_mysql adlerhu/bitcoin_predict >> /tmp/predict.txt
+docker run --name result --net=container:bitcoin_mysql adlerhu/bitcoin_result >> /tmp/result.txt
+docker run --name charts --net=container:bitcoin_mysql -v htmls:/charts/templates/ adlerhu/bitcoin_charts >> /tmp/charts.txt
+docker run --name webapp -d --net=host -v htmls:/webapp/templates/ adlerhu/bitcoin_webapp >> /tmp/webapp.txt
+
+docker rm -f $(docker ps -qf status=exited)" >> /root/daily.sh
+
+# mysql
+touch /root/mysql.yml
+echo "version: '3.3'
+services:
+
+  mysql:
+      image: mysql:8.0
+      container_name: bitcoin_mysql
+      command: mysqld --default-authentication-plugin=mysql_native_password
+      ports: 
+          - 3306:3306
+      environment: 
+          MYSQL_DATABASE: mydb
+          MYSQL_USER: user
+          MYSQL_PASSWORD: hl4su3ao4
+          MYSQL_ROOT_PASSWORD: hl4su3ao4
+      volumes:
+          - mysql:/var/lib/mysql
+      networks:
+          - dev
+
+  phpmyadmin:
+      image: phpmyadmin/phpmyadmin:5.1.0
+      container_name: bitcoin_phphmyadmin
+      links: 
+          - mysql:db
+      ports:
+          - 8000:80
+      depends_on:
+        - mysql
+      networks:
+          - dev
+     
+networks:
+  dev:
+
+volumes:
+  mysql:
+    external: true" >> /root/mysql.yml
+
+docker compose -f /root/mysql.yml up -d
